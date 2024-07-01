cd79ba1a9483c7337b5409a0be764e958503487c
hanifzulmar <hanifzulmar08@gmail.com>
2024-07-01 11:02:35 +0700

    menambah fitur

--- SRC/app.js
+++ SRC/app.js
@@ -1,10 +1,10 @@
 document.addEventListener('alpine:init', () => {
     Alpine.data('products', () => ({
         items: [
-            { id: 1, name: 'Risol Mayo', img: 'risol.jpg', price: 10000},
-            { id: 2, name: 'Cheese Roll', img: 'cheese roll.jpg', price: 10000},
-            { id: 3, name: 'Dimsum', img: 'menu dimsum.jpg', price: 15000},
-            { id: 4, name: 'Piscok', img: 'piscok.jpg', price: 10000}
+            { id: 1, name: 'Risol Mayo', img: '1.jpg', price: 10000},
+            { id: 2, name: 'Cheese Roll', img: '2.jpg', price: 10000},
+            { id: 3, name: 'Dimsum', img: '3.jpg', price: 15000},
+            { id: 4, name: 'Piscok', img: '4.jpg', price: 10000}
         ],
     }));
 
